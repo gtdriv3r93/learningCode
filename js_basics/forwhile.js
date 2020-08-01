2bd835c0c8a305608915e5c2dc283b828efaf3ae
Patricio Vera <patriciovb@ciencias.unam.mx>
2020-08-01 16:26:32 -0500

    Update forwhile.js

--- js_basics/forwhile.js
+++ js_basics/forwhile.js
@@ -35,36 +35,24 @@ function isPrime(num) {
 let numeroPrimo = 0;
 let numeroDePrimos = 0;
 let restoDeNumeros = 0;
-//  for(let index = 2; index <=200000; index++){
-//       numeroPrimo = isPrime(index)
-//       //console.log(index,"Es Primo",numeroPrimo)
-//       if(numeroPrimo == true){
-//          numeroDePrimos++;
-//       }else{
-//         restoDeNumeros++;
-//       }    
-//   }
-//   console.log("Primos", (numeroDePrimos),"- No Primos", (restoDeNumeros))
-numeroPrimo = 0;
+ for(let index = 2; index <=200000; index++){
+      numeroPrimo = isPrime(index)
+      //console.log(index,"Es Primo",numeroPrimo)
+      if(numeroPrimo == true){
+         numeroDePrimos++;
+      }else{
+        restoDeNumeros++;
+      }    
+  }
+  console.log("Primos", (numeroDePrimos),"- No Primos", (restoDeNumeros))
+// numeroPrimo = 0;
 
- let index = 10000;
+//  let index = 10000;
  
- while(numeroPrimo == false){
-    numeroPrimo = isPrime(index)
+//  while(numeroPrimo == false){
+//     numeroPrimo = isPrime(index)
        
-    index++;   
- }
- console.log(index-1,"Es Primo",numeroPrimo)
- //console.log("Primos", (numeroDePrimos),"- No Primos", (restoDeNumeros))
-
- let names=["luke","vader","windu","obi-wan"]
- let weaponColor =["blue","green","red","purple"]
- let side=["dark","light"]
-
- let characters = {
-   name:"example",
-   lightsaber:"example",
-   side:"example",
- }
-
- //crear objetos por cada personaje con sus respectivas caracteristicas, usar for loop, funciones, etc...
+//     index++;   
+//  }
+//  console.log(index-1,"Es Primo",numeroPrimo)
+//  //console.log("Primos", (numeroDePrimos),"- No Primos", (restoDeNumeros))
